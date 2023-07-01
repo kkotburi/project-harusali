ff58084316f03795096fdf33302bedcfeed00051
kkotburi <128236313+kkotburi@users.noreply.github.com>
2023-07-02 02:02:19 +0900

    drafts #21

--- src/components/MyPage/ProfileEditForm.jsx
+++ src/components/MyPage/ProfileEditForm.jsx
@@ -2,16 +2,14 @@
 import { styled } from 'styled-components';
 import { BtnFill } from '../Btn.styled/Btn.style';
 import { InputArea, BtnArea } from '../Users/styled/users.styled';
-import InspectionCaption from '../Users/styled/InspectionCaption';
 // react
-import React, { useState, useRef, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 // firebase
 import { auth, db, storage } from '../../firebase';
-import { collection, doc, setDoc, getDocs, query } from 'firebase/firestore';
-import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { collection, getDocs, query } from 'firebase/firestore';
 import { onAuthStateChanged } from 'firebase/auth';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 
 const ProfileEditForm = () => {
   useEffect(() => {
@@ -38,115 +36,54 @@ const ProfileEditForm = () => {
   }, []);
   // profile image data 확인용
 
-  const users = useSelector((state) => {
-    return state.users;
-  });
+  const [profileimgData, setProfileimgData] = useState(null);
+  const [profileimg, setProfileImg] = useState(null);
+  const [nickName, setNickName] = useState('');
+
+  // image file 올리기
+  const handleFileSelect = (event) => {
+    setProfileimgData(event.target.files[0]);
+  };
+
+  const handleUpload = async () => {
+    const imageRef = ref(storage, `${auth.currentUser.uid}/profileimg/${profileimgData.name}`);
+    await uploadBytes(imageRef, setProfileimgData);
+
+    const downloadURL = await getDownloadURL(imageRef);
+    console.log('downloadURL', downloadURL);
+  };
 
   const dispatch = useDispatch();
-
-  // const [nickName, setNickName] = useState('');
-  // const [profileimg, setProfileImg] = useState(null);
-  // const [profileimgData, setprofileimgData] = useState(null);
-  // const [nickNamestate, setNickNameState] = useState('');
-  // const imageRef = useRef('');
-
-  // const userSetting = async (event) => {
-  //   event.preventDefault();
-  //   const user = auth.currentUser;
-
-  //   if (nickName.length < 3) {
-  //     setNickNameState('none');
-  //     setNickName('');
-  //     return;
-  //   }
-
-  //   let currentNickname = [];
-  //   const userRef = collection(db, 'users');
-  //   const querySnapshot = await getDocs(userRef);
-  //   querySnapshot.forEach((doc) => {
-  //     currentNickname.push(doc.data().userPiece.nickname);
-  //   });
-
-  //   if (currentNickname.indexOf(nickName) >= 0) {
-  //     setNickNameState('overlap');
-  //     setNickName('');
-  //     return;
-  //   }
-
-  //   const newUser = {
-  //     uid: user.uid,
-  //     userPiece: {
-  //       id: user.email,
-  //       nickname: nickName,
-  //       profileimg: ''
-  //     },
-  //     userLike: { follow: [], following: [] }
-  //   };
-
-  //   if (profileimg !== null) {
-  //     const imageRef = ref(storage, `${user.uid}/profileimg/${profileimgData.name}`);
-  //     await uploadBytes(imageRef, profileimgData);
-  //     const downloadUrl = await getDownloadURL(imageRef);
-  //     newUser.userPiece.profileimg = downloadUrl;
-
-  //     const userDocRef = doc(db, 'users', user.uid);
-  //     await setDoc(userDocRef, newUser);
-  //   } else {
-  //     const userDocRef = doc(db, 'users', user.uid);
-  //     await setDoc(userDocRef, newUser);
-  //   }
-  // };
-
-  // const encodeFileTobase64 = (fileBlob) => {
-  //   const reader = new FileReader();
-  //   reader.readAsDataURL(fileBlob);
-  //   return new Promise((resolve) => {
-  //     reader.onload = () => {
-  //       setProfileImg(reader.result);
-  //       resolve();
-  //     };
-  //   });
-  // };
+  // image file 올리기
 
   return (
-    <>
-      {users.filter((user) => {})}
-      {/* <ImgArea
-        profileimg={profileimg ? profileimg : ''}
-        onClick={() => {
-          imageRef.current?.click();
-        }}
-      >
-        {profileimg && <PreviewImg src="{profileimg}" color="red" alt="priview-img" />}
-        <InputArea
-          type="file"
-          style={{ visibility: 'hidden' }}
-          placeholder="프로필 사진을 선택하세요."
-          onChange={(event) => {
-            encodeFileTobase64(event.target.files[0]);
-            setprofileimgData(event.target.files[0]);
-          }}
-          ref={imageRef}
-        ></InputArea>
+    <form
+      onSubnit={(event) => {
+        dispatch({
+          uid: '7m9oJnT0rtURDvLxypCOjs4IFig2',
+          userPiece: {
+            id: '123@123.com',
+            nickname: '123',
+            profileimg:
+              'https://firebasestorage.googleapis.com/v0/b/project-newsfeed-cf73e.appspot.com/o/7m9oJnT0rtURDvLxypCOjs4IFig2%2Fprofileimg%2Fimage%20(1).png?alt=media&token=f8c7fb90-337b-4dc7-b5c0-1e87e82ea22d'
+          },
+          userLike: { follow: [], following: [] }
+        });
+
+        event.preventDefault();
+      }}
+    >
+      <ImgArea>
+        <PreviewImg src={profileimg} />
+        {/* <InputArea type="file" name="image" value={profileimg} onChange={handleFileSelect} /> */}
       </ImgArea>
-      <div>
-        <div></div>
-        <InputArea
-          type="email"
-          placeholder="3자 이상 입력하세요"
-          value={nickName}
-          onChange={(event) => {
-            setNickName(event.target.value);
-          }}
-        ></InputArea>
-      </div>
-      <InspectionCaption nickNamestate={nickNamestate}></InspectionCaption>
+      <InputArea type="text" placeholder={nickName} />
       <BtnArea>
-        <BtnFill size="M" type="submit" onClick={userSetting}>
+        <BtnFill size="M" type="submit" onClick={handleUpload}>
           수정하기
         </BtnFill>
-      </BtnArea> */}
-    </>
+      </BtnArea>
+    </form>
   );
 };
 
@@ -160,6 +97,7 @@ const ImgArea = styled.div`
   margin: 10px 0;
   overflow: hidden;
   position: relative;
+  cursor: pointer;
 
   &:hover {
     background-color: #5763d4;
--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -3,8 +3,18 @@ import BtnSection from '../components/MyPage/BtnSection';
 import CardSection from '../components/MyPage/CardSection';
 import { Inner, SectionContainer } from '../components/mainpage-mypage/main-mypage.styled';
 import MyPageHeader from '../components/MyPage/MyPage.Header';
+import { useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebase';
 
 const Mypage = () => {
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      console.log('user', user);
+    });
+  }, []);
+  // 사용자 정보 확인용
+
   return (
     <Inner>
       <MyPageHeader></MyPageHeader>
